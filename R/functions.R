a55aa5c9a9ac63501131d9f1bb71a539f68c7596
Alex Hayes <alexpghayes@gmail.com>
2021-03-18 13:15:01 -0500

    Update for targets discussion forum

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,71 @@
+flip_coin <- function(p = 0.5) {
+  rbinom(1, 1, p)
+}
 
-model1 <- function(n, k, density) {
+model1 <- function(n, k, expected_degree) {
 
   B <- matrix(0, nrow = k, ncol = k)
+
   diag(B) <- 0.8
-  B[upper.tri(B)] <- 0.3
 
   directed_dcsbm(
-    n = n,
+    theta_in = rep(1, n),
+    theta_out = rep(1, n),
     B = B,
-    expected_density = density
+    expected_density = expected_degree / n
   )
 }
+
+model2 <- function(n, k, expected_degree) {
+
+  B <- matrix(0, nrow = k, ncol = k)
+
+  for (i in 1:k) {
+    for (j in 1:i) {
+
+      if (flip_coin()) {
+        B[i, j] <- 0.4
+      } else {
+        B[j, i] <- 0.4
+      }
+    }
+  }
+
+  diag(B) <- 0.8
+
+  directed_dcsbm(
+    theta_in = rep(1, n),
+    theta_out = rep(1, n),
+    B = B,
+    expected_density = expected_degree / n
+  )
+}
+
+model3 <- function(n, k, expected_degree) {
+
+  B <- matrix(0, nrow = k, ncol = k)
+
+  for (i in 1:k) {
+    for (j in 1:i) {
+
+      if (flip_coin()) {
+        B[i, j] <- 0.4
+      } else {
+        B[j, i] <- 0.4
+      }
+    }
+  }
+
+  diag(B) <- 0.8
+
+  directed_dcsbm(
+    theta_in = 1 + rexp(n, 1/5),
+    theta_out = 1 + rexp(n, 1/5),
+    B = B,
+    expected_density = expected_degree / n
+  )
+}
+
 
 full_svds <- function(A, k) {
   svds(A, k)
@@ -31,10 +86,71 @@ cite_impute <- function(A, k) {
   citation_impute(A_obs, rank = k, max_iter = 20)
 }
 
-subspace_loss <- function(svd_true, svd_estimate, params) {
+plot_matrix <- function(X, ...) {
+  as_tibble(as.matrix(X), rownames = "row") %>%
+    tidyr::gather(col, value, -row) %>%
+    mutate(
+      row = forcats::fct_inseq(row),
+      col = forcats::fct_inseq(col)
+    ) %>%
+    ggplot(aes(x = col, y = row, fill = value)) +
+    geom_raster() +
+    scale_fill_gradient2() +
+    theme_minimal(16) +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank()
+    )
+}
+
+plot_expectation <- function(model, parameters) {
+  E <- model$X %*% tcrossprod(model$S, model$Y)
+  plot <- plot_matrix(E)
+
+  n <- parameters$n
+  k <- parameters$k
+  expected_degree <- parameters$expected_degree
+
+  if (!dir.exists(here("figures/model1_expectations")))
+    dir.create(here("figures/model1_expectations"))
+
+  path <- here(
+    glue("figures/model1_expectations/n={n}_k={k}_expected_degree={expected_degree}.png")
+  )
+
+  ggsave(
+    path,
+    plot = plot,
+    width = 8.5,
+    height = 8.5 * (9/16)
+  )
+
+  path
+}
+
+#' Compute sin-theta distance between subspaces
+#'
+#' @param u An orthogonal basis for a k-dimensional subspace of
+#'   n-dimensional space.
+#' @param v An orthogonal basis for a k-dimensional subspace of
+#'   n-dimensional space.
+#'
+#' @return
+#' @keywords internal
+subspace_loss <- function(u, v) {
+  # see [1] Vu and Lei 2013 section 2.3
+  # and [2] Rohe, Chatterjee, Yu 2011 Annals of Statistics page 1908
+
+  k <- ncol(u)
+
+  s <- svd(crossprod(u, v))
+  k - sum(s$d^2)
+}
+
+loss_helper <- function(svd_true, svd_estimate, params) {
   tibble(
-    u_loss = norm(svd_estimate$u - svd_true$u, type = "F") / 2,
-    v_loss = norm(svd_estimate$v - svd_true$v, type = "F") / 2
+    u_loss = subspace_loss(svd_estimate$u, svd_true$u),
+    v_loss = subspace_loss(svd_estimate$v, svd_true$v)
   ) %>%
     bind_cols(params)
 }
@@ -42,7 +158,7 @@ subspace_loss <- function(svd_true, svd_estimate, params) {
 summarize_losses_model1 <- function(combined_losses) {
   combined_losses %>%
     gather(loss_type, loss, contains("loss")) %>%
-    group_by(estimator, n, k, density, loss_type) %>%
+    group_by(estimator, n, k, expected_degree, loss_type) %>%
     summarize_at(vars(loss), list(~mean(.), ~sd(.))) %>%
     ungroup() %>%
     mutate(
@@ -67,19 +183,19 @@ plot_model1_results <- function(tidied, loss_type = c("u", "v")) {
   plot <- tidied %>%
     filter(loss_type == !!glue("{loss_type}_loss")) %>%
     ggplot() +
-    aes(x = n, y = mean, color = estimator, label = estimator) +
+    aes(x = expected_degree, y = mean, color = estimator, label = estimator) +
     geom_line() +
     geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd)) +
     scale_color_viridis_d(begin = 0.15, end = 0.85, direction = -1) +
     labs(
       title = "Performance of subspace estimators",
       y = glue("{toupper(loss_type)} Loss"),
-      x = "Nodes",
+      x = "Expected degree",
       color = "Estimator"
     ) +
     facet_grid(
       rows = vars(k),
-      cols = vars(density),
+      cols = vars(n),
       scales = "free",
       labeller = label_equals
     ) +
--- _targets.R
+++ _targets.R
@@ -10,43 +10,81 @@ options(
 # remotes::install_github("RoheLab/fastRG")
 
 tar_option_set(
-  packages = c("fastRG", "Matrix", "RSpectra", "cowplot", "glue", "tidyverse", "here")
+  packages = c(
+    "fastRG",
+    "Matrix",
+    "RSpectra",
+    "cowplot",
+    "glue",
+    "tidyverse",
+    "here",
+    "fastadi"
+  )
 )
 
 source(here::here("R/functions.R"))
 
-##### TODO list ----------------------------------------------------------------
-#
-# - Generalize to more than one generative model
-#
-##### END TODO list ------------------------------------------------------------
+library(logger)
+log_threshold(WARN) # hack to hide citation_impute() output
+
+target_models <- tar_map(
+
+  unlist = FALSE,
+
+  values = tibble::tibble(
+    model = rlang::syms(paste0("model", 1:2))
+  ),
+
+  tar_target(
+    distribution,
+    model(parameters$n, parameters$k, parameters$expected_degree),
+    pattern = map(parameters),
+    iteration = "list"
+  ),
+
+  tar_target(
+    realizations,
+    purrr::map(1:num_reps, ~sample_sparse(distribution)),
+    pattern = map(distribution),
+    iteration = "list"
+  ),
+
+  tar_target(
+    svd,
+    svds(distribution),
+    pattern = map(population)
+  )
+)
+
 
 target_estimates <- tar_map(
 
   unlist = FALSE,
 
   values = tibble::tibble(
+
     estimator = rlang::syms(
       c(
         "full_svds",
-        "zero_imputed_svds",
-        "symmetric_svd",
-        "cite_impute"
+        # "zero_imputed_svds",
+        "symmetric_svd" #,
+        # "cite_impute"
       )
     )
   ),
 
-  tar_target(
-    estimate,
-    purrr::map(model1_realizations, ~estimator(.x, k = model1_parameters$k)),
-    pattern = map(model1_realizations, model1_parameters)
-  ),
 
-  tar_target(
-    loss,
-    purrr::map_dfr(estimate, ~subspace_loss(population_svd, .x, params = model1_parameters)),
-    pattern = map(population_svd, estimate, model1_parameters)
-  )
+tar_target(
+  estimate,
+  purrr::map(realizations, ~estimator(.x, k = parameters$k)),
+  pattern = map(realizations, parameters)
+)
+
+  # tar_target(
+  #   loss,
+  #   purrr::map_dfr(estimate, ~loss_helper(population_svd, .x, params = parameters)),
+  #   pattern = map(population_svd, estimate, parameters)
+  # )
 )
 
 target_combined <- tar_combine(
@@ -55,60 +93,54 @@ target_combined <- tar_combine(
   command = dplyr::bind_rows(!!!.x, .id = "estimator")
 )
 
-num_reps <- 30
-
 list(
 
-  tar_target(n, c(100, 250, 500)), #, 1000, 2500)),
+  tar_target(num_reps, 2),
+
+  tar_target(n, c(500, 1000)), #, 2500)),
+
+  tar_target(expected_degree, c(10, 25, 50, 100, 200)),
 
   tar_target(k, c(2, 5, 10)),
 
-  tar_target(density, c(0.1, 0.2, 0.3, 0.4)),
-
   tar_target(
-    model1_parameters,
-    tibble::tibble(n = n, k = k, density = density),
-    pattern = cross(n, k , density)
-    # tidyr::expand_grid(n, k, density)
+    parameters,
+    tibble::tibble(n = n, k = k, expected_degree = expected_degree),
+    pattern = cross(n, k, expected_degree)
   ),
 
-  tar_target(
-    population_graph,
-    model1(model1_parameters$n, model1_parameters$k, model1_parameters$density),
-    pattern = map(model1_parameters),
-    iteration = "list"
-  ),
 
-  tar_target(
-    model1_realizations,
-    purrr::map(1:num_reps, ~sample_sparse(population_graph)),
-    pattern = map(population_graph),
-    iteration = "list"
-  ),
 
-  tar_target(
-    population_svd,
-    svds(population_graph),
-    pattern = map(population_graph)
-  ),
+  # tar_target(
+  #   expectation_plots,
+  #   plot_expectation(population_graph, parameters),
+  #   pattern = map(population_graph, parameters),
+  #   format = "file"
+  # ),
 
-  target_estimates,
 
-  target_combined,
 
-  tar_target(
-    model1_summarized_losses,
-    summarize_losses_model1(combined_losses)
-  ),
+  target_models,
 
-  tar_target(
-    model1_u_loss_plot,
-    plot_model1_results(model1_summarized_losses, loss_type = "u")
-  ),
-
-  tar_target(
-    model1_v_loss_plot,
-    plot_model1_results(model1_summarized_losses, loss_type = "v")
-  )
+  target_estimates
+#
+#   target_combined,
+#
+#   tar_target(
+#     model1_summarized_losses,
+#     summarize_losses_model1(combined_losses)
+#   ),
+#
+#   tar_target(
+#     model1_u_loss_plot,
+#     plot_model1_results(model1_summarized_losses, loss_type = "u"),
+#     format = "file"
+#   ),
+#
+#   tar_target(
+#     model1_v_loss_plot,
+#     plot_model1_results(model1_summarized_losses, loss_type = "v"),
+#     format = "file"
+#   )
 )
 
